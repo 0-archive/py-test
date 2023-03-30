c33c30f38910a6330f968525fee44e28b832838d
steven <schaefsteven@gmail.com>
2023-03-30 10:19:54 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+""" Objective: Traverse a matrix of integers in a spiral pattern and append the integers to a 1-dimensional array in order.
+Example:
+    input = [
+    [ 1,  2,  3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10,  9,  8, 7],
+    ]
+    output = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] """
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":
+    main()
